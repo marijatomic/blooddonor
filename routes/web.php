947e2f2f7d4ceb7eae056e02721da6d9d5564faf
BrigS94 <brigita.s@live.com>
2018-05-24 07:22:03 +0200

    Templates

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/index','LandPage1Controller@index');
+Route::get('/index1','LandPage2Controller@index');
+Route::get('/index2','LandPage3Controller@index');
+
